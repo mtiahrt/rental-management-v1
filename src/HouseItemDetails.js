a3f2f34ddba38f172e34b458de25355e6170184c
Mark Tiahrt <mark.tiahrt@outlook.com>
2018-04-21 10:59:32 -0600

    hardcoded data for offline work

--- src/HouseItemDetails.js
+++ src/HouseItemDetails.js
@@ -6,7 +6,308 @@ import { Jumbotron, Card, Button, CardImg, CardTitle, CardText,
 import {withRouter} from 'react-router-dom';
 import TenantItem from './TenantItem';
 
-let data;
+let data = {
+    "propertyByPropertyid": {
+      "propertyid": 1,
+      "propertyaddress": "2105 8th Ave",
+      "propertycity": "Greeley",
+      "propertystate": "CO",
+      "propertyunitsByPropertyid": {
+        "edges": [
+          {
+            "node": {
+              "numberofbedrooms": 2,
+              "description": "2105 Upstairs",
+              "unitnumber": 1,
+              "unitid": 1,
+              "leasesByUnitid": {
+                "edges": [
+                  {
+                    "node": {
+                      "leaseid": 16,
+                      "unitid": 1,
+                      "rentamount": 750,
+                      "startDate": "2015-06-01",
+                      "endDate": "2016-05-27",
+                      "tenantsByLeaseid": {
+                        "edges": [
+                          {
+                            "node": {
+                              "tenantid": 15,
+                              "leaseid": 16,
+                              "nameid": 40,
+                              "parentalguarantee": false,
+                              "nameByNameid": {
+                                "nameid": 40,
+                                "firstname": "Huber Sprinkler",
+                                "lastname": null,
+                                "isMale": null
+                              }
+                            }
+                          },
+                          {
+                            "node": {
+                              "tenantid": 16,
+                              "leaseid": 16,
+                              "nameid": 39,
+                              "parentalguarantee": false,
+                              "nameByNameid": {
+                                "nameid": 39,
+                                "firstname": "Maffat Paint and Glass",
+                                "lastname": null,
+                                "isMale": null
+                              }
+                            }
+                          }
+                        ]
+                      }
+                    }
+                  },
+                  {
+                    "node": {
+                      "leaseid": 29,
+                      "unitid": 1,
+                      "rentamount": 725,
+                      "startDate": "2014-06-01",
+                      "endDate": "2015-05-25",
+                      "tenantsByLeaseid": {
+                        "edges": []
+                      }
+                    }
+                  },
+                  {
+                    "node": {
+                      "leaseid": 44,
+                      "unitid": 1,
+                      "rentamount": 710,
+                      "startDate": "2013-04-01",
+                      "endDate": "2014-05-30",
+                      "tenantsByLeaseid": {
+                        "edges": []
+                      }
+                    }
+                  },
+                  {
+                    "node": {
+                      "leaseid": 51,
+                      "unitid": 1,
+                      "rentamount": 750,
+                      "startDate": "2016-06-01",
+                      "endDate": "2017-05-31",
+                      "tenantsByLeaseid": {
+                        "edges": [
+                          {
+                            "node": {
+                              "tenantid": 79,
+                              "leaseid": 51,
+                              "nameid": 89,
+                              "parentalguarantee": false,
+                              "nameByNameid": {
+                                "nameid": 89,
+                                "firstname": "Cole",
+                                "lastname": "Hendrickson",
+                                "isMale": null
+                              }
+                            }
+                          },
+                          {
+                            "node": {
+                              "tenantid": 80,
+                              "leaseid": 51,
+                              "nameid": 90,
+                              "parentalguarantee": false,
+                              "nameByNameid": {
+                                "nameid": 90,
+                                "firstname": "Graham's Flooring",
+                                "lastname": null,
+                                "isMale": null
+                              }
+                            }
+                          }
+                        ]
+                      }
+                    }
+                  },
+                  {
+                    "node": {
+                      "leaseid": 64,
+                      "unitid": 1,
+                      "rentamount": 800,
+                      "startDate": "2017-06-01",
+                      "endDate": "2018-05-31",
+                      "tenantsByLeaseid": {
+                        "edges": [
+                          {
+                            "node": {
+                              "tenantid": 112,
+                              "leaseid": 64,
+                              "nameid": 106,
+                              "parentalguarantee": false,
+                              "nameByNameid": {
+                                "nameid": 106,
+                                "firstname": "IPM Pest Management",
+                                "lastname": null,
+                                "isMale": null
+                              }
+                            }
+                          }
+                        ]
+                      }
+                    }
+                  }
+                ]
+              }
+            }
+          },
+          {
+            "node": {
+              "numberofbedrooms": 2,
+              "description": "2105 Downstairs",
+              "unitnumber": 2,
+              "unitid": 2,
+              "leasesByUnitid": {
+                "edges": [
+                  {
+                    "node": {
+                      "leaseid": 17,
+                      "unitid": 2,
+                      "rentamount": 700,
+                      "startDate": "2015-06-01",
+                      "endDate": "2016-05-27",
+                      "tenantsByLeaseid": {
+                        "edges": [
+                          {
+                            "node": {
+                              "tenantid": 40,
+                              "leaseid": 17,
+                              "nameid": 71,
+                              "parentalguarantee": false,
+                              "nameByNameid": {
+                                "nameid": 71,
+                                "firstname": "Shur Painting",
+                                "lastname": null,
+                                "isMale": null
+                              }
+                            }
+                          },
+                          {
+                            "node": {
+                              "tenantid": 41,
+                              "leaseid": 17,
+                              "nameid": 70,
+                              "parentalguarantee": false,
+                              "nameByNameid": {
+                                "nameid": 70,
+                                "firstname": "Boxelder Sanitation",
+                                "lastname": null,
+                                "isMale": null
+                              }
+                            }
+                          }
+                        ]
+                      }
+                    }
+                  },
+                  {
+                    "node": {
+                      "leaseid": 45,
+                      "unitid": 2,
+                      "rentamount": 620,
+                      "startDate": "2013-04-01",
+                      "endDate": "2014-05-30",
+                      "tenantsByLeaseid": {
+                        "edges": []
+                      }
+                    }
+                  },
+                  {
+                    "node": {
+                      "leaseid": 52,
+                      "unitid": 2,
+                      "rentamount": 750,
+                      "startDate": "2016-06-01",
+                      "endDate": "2017-05-31",
+                      "tenantsByLeaseid": {
+                        "edges": [
+                          {
+                            "node": {
+                              "tenantid": 77,
+                              "leaseid": 52,
+                              "nameid": 91,
+                              "parentalguarantee": false,
+                              "nameByNameid": {
+                                "nameid": 91,
+                                "firstname": "Ben Mendoza",
+                                "lastname": null,
+                                "isMale": null
+                              }
+                            }
+                          },
+                          {
+                            "node": {
+                              "tenantid": 78,
+                              "leaseid": 52,
+                              "nameid": 92,
+                              "parentalguarantee": false,
+                              "nameByNameid": {
+                                "nameid": 92,
+                                "firstname": "Property Techinca",
+                                "lastname": null,
+                                "isMale": null
+                              }
+                            }
+                          }
+                        ]
+                      }
+                    }
+                  },
+                  {
+                    "node": {
+                      "leaseid": 65,
+                      "unitid": 2,
+                      "rentamount": 800,
+                      "startDate": "2018-01-16",
+                      "endDate": "2018-01-16",
+                      "tenantsByLeaseid": {
+                        "edges": []
+                      }
+                    }
+                  },
+                  {
+                    "node": {
+                      "leaseid": 66,
+                      "unitid": 2,
+                      "rentamount": 800,
+                      "startDate": "2017-06-01",
+                      "endDate": "2018-05-31",
+                      "tenantsByLeaseid": {
+                        "edges": [
+                          {
+                            "node": {
+                              "tenantid": 113,
+                              "leaseid": 66,
+                              "nameid": 106,
+                              "parentalguarantee": false,
+                              "nameByNameid": {
+                                "nameid": 106,
+                                "firstname": "IPM Pest Management",
+                                "lastname": null,
+                                "isMale": null
+                              }
+                            }
+                          }
+                        ]
+                      }
+                    }
+                  }
+                ]
+              }
+            }
+          }
+        ]
+      }
+    }
+  };
 let tenants;
 const query = gql `query(
   $propertyid: Int!
@@ -30,6 +331,8 @@ const query = gql `query(
                 leaseid
                 unitid
                 rentamount
+                startDate
+                endDate
                 tenantsByLeaseid(condition: {flagdeleted: false}){
                   edges{
                     node{
@@ -97,14 +400,15 @@ class HouseItemDetails extends Component {
   }
 
     render(){
-        data = this.props.data
+        // data = this.props.data
         if (data.loading) {
             return <div>Loading...</div>
           }
           if (data.error) {
             return <div>{data.error}</div>
           }
-          data = this.props.data.propertyByPropertyid;
+          // data = this.props.data.propertyByPropertyid;
+          data = data.propertyByPropertyid
         return(
         <div>      
           <Jumbotron>
@@ -145,4 +449,5 @@ const HouseItemDetailsWithData = graphql(query, {
   options: ownProps => ({variables: {propertyid: ownProps.match.params.propertyId}}),
 })(HouseItemDetails);
 
-export default withRouter(HouseItemDetailsWithData);
+// export default withRouter(HouseItemDetailsWithData);
+export default HouseItemDetails;
--- src/Houses.js
+++ src/Houses.js
@@ -5,6 +5,79 @@ import {Row, Col} from 'reactstrap';
 import HouseItem from './HouseItem';
 import {Link} from 'react-router-dom';
 
+
+
+let data = {
+    "allProperties": {
+      "edges": [
+        {
+          "node": {
+            "propertyid": 1,
+            "propertyaddress": "2105 8th Ave",
+            "propertycity": "Greeley",
+            "propertystate": "CO",
+            "imageurl": "https://i.imgur.com/iExZVA6.jpg"
+          }
+        },
+        {
+          "node": {
+            "propertyid": 2,
+            "propertyaddress": "1917 8th Ave",
+            "propertycity": "Greeley",
+            "propertystate": "CO",
+            "imageurl": "https://i.imgur.com/HLs7uMc.jpg"
+          }
+        },
+        {
+          "node": {
+            "propertyid": 3,
+            "propertyaddress": "1833 11th Ave",
+            "propertycity": "Greeley",
+            "propertystate": "CO",
+            "imageurl": "https://i.imgur.com/CsTFXkG.jpg"
+          }
+        },
+        {
+          "node": {
+            "propertyid": 4,
+            "propertyaddress": "5551 W. 29st",
+            "propertycity": "Greeley",
+            "propertystate": "CO",
+            "imageurl": "https://i.imgur.com/l8i3Gfq.jpg"
+          }
+        },
+        {
+          "node": {
+            "propertyid": 5,
+            "propertyaddress": "705 W. Prospect",
+            "propertycity": "Fort Collins",
+            "propertystate": "CO",
+            "imageurl": "https://i.imgur.com/CGS5tgL.jpg"
+          }
+        },
+        {
+          "node": {
+            "propertyid": 6,
+            "propertyaddress": "1002 Lochmore Pl",
+            "propertycity": "Fort Collins",
+            "propertystate": "CO",
+            "imageurl": "https://i.imgur.com/gZtgwDr.jpg"
+          }
+        },
+        {
+          "node": {
+            "propertyid": 7,
+            "propertyaddress": "2921 Stonehaven Dr",
+            "propertycity": "Fort Collins",
+            "propertystate": "CO",
+            "imageurl": "https://i.imgur.com/YRbF7ed.jpg"
+          }
+        }
+      ]
+    }
+  }
+
+
 const query = gql`
 {allProperties(condition: {flagdeleted: false}){
   edges{
@@ -21,7 +94,7 @@ const query = gql`
 `;
 class Houses extends React.Component {
     render(){
-    let data = this.props.data
+    // let data = this.props.data
     if (data.loading) {
       return <div>Loading...</div>
     }
@@ -41,5 +114,5 @@ class Houses extends React.Component {
     )}
 }
 
-Houses = graphql(query) (Houses)
+// Houses = graphql(query) (Houses)
 export default Houses;
